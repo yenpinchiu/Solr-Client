11601dc41973811391744eeacab8a0762f5e926f
YenPin Chiu (DCS-TW) <yenpin_chiu@tw-yenpinchiu.client.tw.trendnet.org>
2018-01-24 11:12:10 +0800

    [REFACTOR] small refactor

--- solr_client.py
+++ solr_client.py
@@ -2,12 +2,13 @@ import requests
 import json
 
 UPDATE_HANDLER = "update/json?commit=true"
+SOLR_URL = "http://{}:{}/solr"
 
 class Solr_client:
     def __init__(self, host="localhost", port=8983):
         self.host = host
         self.port = port
-        self.url = "http://{}:{}/solr".format(host, port)
+        self.url = SOLR_URL.format(host, port)
         self.update_handler = UPDATE_HANDLER
         
     def update(self, collection_name, data):
@@ -15,7 +16,6 @@ class Solr_client:
         data (dict): A dict object which will be updated to the specific solr collection as a structured document. 
         collection_name(str): Name of the target collection.
         '''
-
         post_data_str = str(json.dumps([data]))
         post_url = "{}/{}/{}".format(self.url, collection_name, self.update_handler)
 
--- test_run.py
+++ test_run.py
@@ -1,27 +1,9 @@
-import solr_client as sc
-import random, string
-
-class random_test_case:
-    def __init__(self, seed=0, fields_scheme={"name"}):
-        '''
-        fields_scheme(set): fields name of generated documents.
-        '''
-        random.seed(seed)
-        self.fields_scheme = fields_scheme
-    
-    def random_str(self, max_str_len):
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(1, max_str_len)))
-
-    def generate_random_case(self, max_str_len =5):
-        random_case = {}
-        print(self.fields_scheme)
-        for field_name in self.fields_scheme:
-            random_case.update({field_name: self.random_str(max_str_len)})
-        return random_case
+import solr_client
+import random_test_case
 
 if __name__ == "__main__":
-    solr_client = sc.Solr_client()
-    rand_case = random_test_case(fields_scheme={"name", "field_0", "field_1", "field_2"})
+    sc = solr_client.Solr_client()
+    rtc = random_test_case.random_test_case(fields_scheme={"name", "field_0", "field_1", "field_2"})
     for i in range(0, 10):
-        res = solr_client.update("Test-Collection", rand_case.generate_random_case())
+        res = sc.update("Test-Collection", rtc.generate_random_case())
         print(res)
