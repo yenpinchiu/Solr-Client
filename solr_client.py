23538e767004558b400c6f515cb021c3dbd4864e
YenPin Chiu (DCS-TW) <yenpin_chiu@tw-yenpinchiu.client.tw.trendnet.org>
2018-01-24 10:55:20 +0800

    [FEAT] First prototype version with test case generator.

---
+++ solr_client.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+UPDATE_HANDLER = "update/json?commit=true"
+
+class solr_client:
+    def __init__(self, host="localhost", port=8983):
+        self.host = host
+        self.port = port
+        self.url = "http://{}:{}/solr".format(host, port)
+        self.update_handler = UPDATE_HANDLER
+        
+    def update(self, collection_name, data):
+        '''
+        data (dict): A dict object which will be updated to the specific solr collection as a structured document. 
+        collection_name(str): Name of the target collection.
+        '''
+
+        post_data_str = str(json.dumps([data]))
+        post_url = "{}/{}/{}".format(self.url, collection_name, self.update_handler)
+
+        res = requests.post(post_url, data=post_data_str)
+        return res.text
